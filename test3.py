0faedb58080a0503b0ff789e650fb948df2f0fee
user <irin-shkuratov8@yandex.ru>
2025-03-28 18:42:24 +0300

    /

--- test3.py
+++ test3.py
@@ -1,14 +1,27 @@
-s = "йцукенгшщзхъфывапролджэячсмитьбю"
-ar = {}
-with open("songs.txt", "r", encoding="utf-8") as f:
-    text = f.readlines()
+import sqlite3
+
+
+connection = sqlite3.connect('music.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Music (
+id INTEGER PRIMARY KEY,
+name TEXT,
+text TEXT,
+notes TEXT
+)
+''')
+
+with open("songs2.txt", "r", encoding="utf-8") as file:
+    text = file.readlines()
     for i in text:
-        new_i = i.split("|")[-1]
-        cnt = 0
-        for j in range(100):
-            if new_i[j] not in s:
-                cnt += 1
-        if cnt < 30:
-            with open("songs2.txt", "a", encoding="utf-8") as f2:
-                f2.write(i)
+        i = i.strip("\n")
+        if len(i.split("|")) == 2:
+            name, music = i.split("|")
+            cursor.execute('INSERT INTO Music (name, text, notes) VALUES (?, ?, ?)', (name, music, ""))
+
+
+connection.commit()
+connection.close()
 print("Ok")
